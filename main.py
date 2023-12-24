143a71b0704dcf9cdcecaf4b8f2cc82190edcd42
TomBad13 <122915278+TomBad13@users.noreply.github.com>
2023-12-24 01:10:57 +0100

    main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ if len(sys.argv) > 1:
     mode = sys.argv[1]    
     fils = sys.argv[2]
     if mode == "1":
-        print("Mode terminal choisi, veuillez choisir votre nombre de fils")
         subprocess.run(["python", "Console.py", fils])
     elif mode == "2":
-        print("Mode terminal choisi, veuillez choisir votre nombre de fils")
         subprocess.run(["python", "GUI.py", fils])
